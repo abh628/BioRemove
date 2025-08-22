ac2db4527c0716a88479fadba92016abcb1ade85
strad-dev131 <stradiogamer@gmail.com>
2025-08-22 14:41:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ DEFAULT_WARNING_LIMIT = 3
 DEFAULT_PUNISHMENT = "mute" # Options: "mute", "ban"
 DEFAULT_CONFIG = ("warn", DEFAULT_WARNING_LIMIT, DEFAULT_PUNISHMENT)
 
-# Regex pattern to detect URLs in user bios
+# Regex pattern to detect URLs and @mentions in user bios
 URL_PATTERN = re.compile(
-    r'(https?://|www\.)[a-zA-Z0-9.\-]+(\.[a-zA-Z]{2,})+(/[a-zA-Z0-9._%+-]*)*' #done change here
+    r'(https?://|www\.)[a-zA-Z0-9.\-]+(\.[a-zA-Z]{2,})+(/[a-zA-Z0-9._%+-]*)*|@[\w_]+'
 )
